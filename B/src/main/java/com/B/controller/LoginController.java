d3f928dc6d1eada879b71bc8aaafebf8021aa060
Jeongwonwoo1996 <84284474+Jeongwonwoo1996@users.noreply.github.com>
2021-10-21 15:49:32 +0900

    비밀번호 오류 수정

--- B/src/main/java/com/B/controller/LoginController.java
+++ B/src/main/java/com/B/controller/LoginController.java
@@ -9,7 +9,7 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
